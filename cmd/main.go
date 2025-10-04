9b6db9296422277b6171dc0693344356932e8b33
aoutrgua <outrgua1949@gmail.com>
2025-10-04 16:14:06 +0100

    update style & add login func & style func

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"net/http"
+
+	"forum/handlers"
+)
+
+func main() {
+	http.HandleFunc("/static/", handlers.StyleFunc)
+	http.HandleFunc("/login", handlers.HanldlerLogin)
+	fmt.Println("server is runing http://localhost:8085/login")
+	http.ListenAndServe(":8085",nil)
 }
