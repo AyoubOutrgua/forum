31af804e03260ccc607981953aaa773066541b1f
boulhaj <awlhajb@gmail.com>
2025-10-11 12:47:34 +0100

    updating database

--- database/data.go
+++ database/data.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func CreateUser() {
+	dataBase, err := sql.Open("sqlite3", "forum.db")
+	if err != nil {
+		log.Fatal()
+	}
+	defer dataBase.Close()
+	usersTable := `CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userName TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE ,
+    password TEXT NOT NULL
+);`
+	_, err = dataBase.Exec(usersTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
