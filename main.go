6720e7e79b402d4361104c872f509591580b82ae
basma <basmatoumanar55@gmail.com>
2025-10-24 11:22:40 +0100

    add posts

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
 	"time"
-	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -19,23 +20,41 @@ type Comment struct {
 	Dislikes int
 }
 
+type Post struct {
+	ID       int
+	Title    string
+	Content  string
+	Author   string
+	Date     string
+	Comments []Comment // slice dial comments per post
+}
+
+//var posts []Post
+
 func main() {
 	// On se connecte à la base SQLite existante
-	db, err := sql.Open("sqlite3", "./file.db") // sqlite3 is a driver (motrjim) , sql open 
-	if err != nil {
-		panic("Erreur lors de l'ouverture de file.db : " + err.Error())
+	dbcomment, err1 := sql.Open("sqlite3", "./comment.db") // sqlite3 is a driver (motrjim) , sql open
+	if err1 != nil {
+		panic("Erreur lors de l'ouverture de file.db : " + err1.Error())
 	}
-	defer db.Close() // kanssedo l connexion li mabin go u file.db 
+	defer dbcomment.Close() // kanssedo l connexion li mabin go u file.db
+	// On se connecte à la base SQLite existante
+	dbpost, err2 := sql.Open("sqlite3", "./post.db") // daba ghi post.db
+	if err2 != nil {
+		panic("Erreur lors de l'ouverture post.db : " + err2.Error())
+	}
+	defer dbpost.Close()
 
 	// Route principale
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		Home(w, r, db)
+		Home(w, r, dbcomment, dbpost)
 	})
 
 	fmt.Println("Serveur lancé sur : http://localhost:8080/home")
 	http.ListenAndServe(":8080", nil)
 }
-func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+func Home(w http.ResponseWriter, r *http.Request, db *sql.DB, Pdb *sql.DB) {
 	switch r.Method {
 	case http.MethodPost:
 		text := r.FormValue("text")
@@ -64,7 +83,7 @@ func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 			INSERT INTO comments (text, author, post_id, date, likes, dislikes) 
 			VALUES (?, ?, ?, ?, ?, ?)`,
 			text, author, postId, date, likes, dislikes,
-		) 
+		)
 		if err != nil {
 			http.Error(w, "500 internal server error: insertion échouée", http.StatusInternalServerError)
 			return
@@ -74,27 +93,52 @@ func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		return
 
 	case http.MethodGet:
-		rows, err := db.Query("SELECT text, author, post_id, date, likes, dislikes FROM comments") //This is the line that "brings back" everything we have stored in the DB to use in our program.
+		// 1️⃣ Jibou comments men DB
+		rows, err := db.Query("SELECT text, author, post_id, date, likes, dislikes FROM comments")
 		if err != nil {
-			http.Error(w, "500 internal server error: lecture échouée: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "500 internal server error: lecture comments échouée: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer rows.Close()
 
 		var comments []Comment
-		for rows.Next() { // like we have 3 rows if we are in the first one rows.next = true 
+		for rows.Next() {
 			var c Comment
-			rows.Scan(&c.Text, &c.Author, &c.PostID, &c.Date, &c.Likes, &c.Dislikes) //copie les valeurs de la row de la DB dans la struct Go.
+			rows.Scan(&c.Text, &c.Author, &c.PostID, &c.Date, &c.Likes, &c.Dislikes)
 			comments = append(comments, c)
 		}
-		// If I change the order in the SELECT, I must also change the order of the variables in Scan, otherwise I will have data in the wrong columns.
+
+		// 2️⃣ Jibou posts men DB
+		postRows, err := Pdb.Query("SELECT id, title, content, author, date FROM posts")
+		if err != nil {
+			http.Error(w, "500 internal server error: lecture posts échouée: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer postRows.Close()
+
+		var posts []Post
+		for postRows.Next() {
+			var p Post
+			postRows.Scan(&p.ID, &p.Title, &p.Content, &p.Author, &p.Date)
+
+			// 3️⃣ Attach comments li kaynin f had post
+			for _, c := range comments {
+				if c.PostID == p.ID {
+					p.Comments = append(p.Comments, c)
+				}
+			}
+
+			posts = append(posts, p)
+		}
+
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			http.Error(w, "500 internal server error: template introuvable", http.StatusInternalServerError)
 			return
 		}
 
-		tmpl.Execute(w, map[string]interface{}{"Comments": comments})
+		tmpl.Execute(w, map[string]interface{}{"Posts": posts})
+
 	default:
 		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
 	}
