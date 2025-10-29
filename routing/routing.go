17c83c6415d9139030e7f1de3334cfd6fbe0e892
btoumana <basmatoumanar55@gmail.com>
2025-10-29 14:56:31 +0100

    add comments handler

--- routing/routing.go
+++ routing/routing.go
@@ -11,7 +11,7 @@ import (
 func Routing() {
 	manager := middleware.NewRateLimiterManager(10, 1*time.Minute)
 	registerLimiter := middleware.NewRateLimiterManager(10, 1*time.Minute)
-
+	http.HandleFunc("/createcomment", handlers.CreateCommentHandler)
 	http.HandleFunc("/login", middleware.RateLimitLoginMiddleware(manager, handlers.LoginHandler))
 	http.HandleFunc("/register", middleware.RateLimitLoginMiddleware(registerLimiter, handlers.RegisterHandler))
 	http.HandleFunc("/static/", handlers.StyleFunc)
