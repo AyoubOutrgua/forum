fa2ca780d509ed0cff8473a9e6e83bdadab8f768
Ayoub Outrgua <74433630+Ayoub-Outrgua@users.noreply.github.com>
2025-11-08 11:58:39 +0000

    add RateLimitComments middleware to manage comment submission frequency

---
+++ middleware/rateLimitComments.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"forum/helpers"
+)
+
+func RateLimitComments(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, errSession := r.Cookie("session")
+		if errSession != nil || cookie.Value == "" {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		cookieID := cookie.Value
+
+		userID := helpers.GetUserID(cookieID)
+
+		dates := helpers.GetLastCommentsDates(userID)
+		if len(dates) == 5 {
+			firstDateTime, _ := time.Parse("2006-01-02 15:04:05", dates[len(dates)-1])
+			dateNowTime := time.Now()
+
+			if dateNowTime.Sub(firstDateTime).Minutes() <= 1 {
+				helpers.Errorhandler(w, "Status Too Many Requests", http.StatusTooManyRequests)
+				return
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
