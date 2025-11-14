f9e611c1799e28988a0f6052059454c48eec3288
mohammedhill <howasimo57@gmail.com>
2025-11-14 12:07:34 +0100

    Refactor code structure for improved readability and maintainability

--- routing/routing.go
+++ routing/routing.go
@@ -12,10 +12,13 @@ func Routing() {
 	manager := middleware.NewRateLimiterManager(10, 1*time.Minute)
 	registerLimiter := middleware.NewRateLimiterManager(10, 1*time.Minute)
 
-	http.HandleFunc("/login", middleware.RateLimitLoginMiddleware(manager, handlers.LoginHandler))
-	http.HandleFunc("/register", middleware.RateLimitLoginMiddleware(registerLimiter, handlers.RegisterHandler))
+	http.HandleFunc("/loginAuth", middleware.RateLimitLoginMiddleware(manager, handlers.LoginHandler))
+	http.HandleFunc("/register", handlers.Showregister)
+	http.HandleFunc("/login", handlers.Showloginhandler)
+
+	http.HandleFunc("/registerAuth",middleware.RateLimitLoginMiddleware(registerLimiter, handlers.RegisterHandler))
 	http.HandleFunc("/static/", handlers.StyleFunc)
 	http.HandleFunc("/", handlers.HanldlerShowHome)
-	http.HandleFunc("/createpost", middleware.RateLimitPost(handlers.CreatePostHandler))
+	http.HandleFunc("/createpost",middleware.Checksession( middleware.RateLimitPost(handlers.CreatePostHandler)))
 	http.HandleFunc("/logout", handlers.LogOutHandler)
 }
